b90b5011128f84a1a0f7b48f0f4bf3e13423a247
Paula Siauchó Unriza <pm.siaucho1305@uniandes.edu.co>
2015-06-03 18:51:19 -0500

    lotería

--- Talleres/Taller2/loteria.sh
+++ Talleres/Taller2/loteria.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+wget -q https://raw.githubusercontent.com/ComputoCienciasUniandes/MetodosComputacionalesLaboratorio/master/2015-V/actividades/lottery/lottery.csv
+echo "El código:"
+grep 201126749 lottery.csv
+echo "en la fecha:"
+head -1 lottery.csv
 
-grep 201126749 lottery.txt
